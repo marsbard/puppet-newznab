70ac4010e1992821f0e64c6b96325ad1975b2e1a
marsbard <martin@ciderpunk.com>
2014-11-01 23:09:32 +0100

    fixing module file path

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,7 +1,7 @@
 class mysql {
 
 	file { "/etc/mysql/my.cnf" :
-		source => "puppet:///mysql/my.cnf",
+		source => "puppet:///modules/mysql/my.cnf",
 		ensure => "present",
 	}
 
--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,12 +1,12 @@
 class php {
 
 	file { "/etc/php5/apache2/php.ini":
-		source => "puppet:///php/etc-php5-apache2-php.ini",
+		source => "puppet:///modules/php/etc-php5-apache2-php.ini",
 		ensure => "present",
 	}
 
 	file { "/etc/php5/cli/php.ini":
-		source => "puppet:///php/etc-php5-cli-php.ini",
+		source => "puppet:///modules/php/etc-php5-cli-php.ini",
 		ensure => "present",
 	}
 
